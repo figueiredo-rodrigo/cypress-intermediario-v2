df9342cd117918f6ad038bd1e832acabf98b09c7
figueiredo-rodrigo <rodrigo8862@gmail.com>
2023-01-09 22:01:17 -0300

    Criar projeto cypress-intermediário

---
+++ cypress/support/cli_commands.js
@@ -0,0 +1,6 @@
+Cypress.Commands.add('cloneViaSSH', project => {
+    const domain = Cypress.config('baseUrl').replace('http://', '')
+
+    //cy.exec('type nul > “nomedoarquivo.txt”')
+    cy.exec(`git clone git@${domain}:${Cypress.env('user_name')}/${project.name}.git`)
+  })
